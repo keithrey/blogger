ad677fcd2b3a6d9cc0e4ec9217137688a12ffa2b
keithrey <95947447+keithrey@users.noreply.github.com>
2021-12-24 16:10:25 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 {
   "items": [
-    {"url": "https://bit.ly/3oJg5wF"},
-    {"url": "https://bit.ly/3yhj9Db"}
+    {"url": "https://scontent.fmnl26-2.fna.fbcdn.net/v/t39.30808-6/fr/cp0/e15/q65/266398657_101145859095254_8358018995382093230_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=85a577&efg=eyJpIjoidCJ9&_nc_eui2=AeHAl6PeXIgIJCBpuJ90E94b7pzSfM0e9LXunNJ8zR70teyoaPee7DqmbNML2I2KZLRcm5jXFwvTPg5XPr_Zgjfh&_nc_ohc=OgUf_HEbuEoAX_nSy33&_nc_zt=23&_nc_ht=scontent.fmnl26-2.fna&oh=00_AT-up786qHVV03jnSwAMdWwI63KMHtXEq4GXDXLpGxlMJA&oe=61CA6679"},
+    {"url": "https://scontent.fmnl26-1.fna.fbcdn.net/v/t39.30808-6/fr/cp0/e15/q65/265814732_101137565762750_2549103753938412976_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=dd9801&efg=eyJpIjoidCJ9&_nc_eui2=AeHIOZzI7wHbm8uo9N_Poki8n4Sf9pwsIbefhJ_2nCwht6FY8OCLCAkoxsRsZZX7aVbOmOIvL2tm1f6zZpyQv5_o&_nc_ohc=_w56F8UXbXcAX8XB4-y&_nc_zt=23&_nc_ht=scontent.fmnl26-1.fna&oh=00_AT_BrDLraaIw6HeVkf-LhMqzd_GjdzhaaHRXl73v6wz8XQ&oe=61CB47B7"}
    ]
 }
